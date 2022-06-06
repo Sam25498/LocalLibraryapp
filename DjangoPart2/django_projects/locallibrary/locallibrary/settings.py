458c25959941dd328f69019f5fb4ecf1cea04efa
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-06 10:01:40 +0300

    Create settings.py

---
+++ DjangoPart2/django_projects/locallibrary/locallibrary/settings.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'catalog.apps.CatalogConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Add our new application
+    'catalog.apps.CatalogConfig', #This object was created for us in /catalog/apps.py
+]
