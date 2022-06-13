09e0203b1a461f48fdc6c172c47de6b5a8387f65
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-13 14:05:45 +0300

    Create views.py

---
+++ DjangoPart7/django_projects/locallibrary/catalog/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    
+    num_genres = Genre.objects.filter(name__icontains='f').count()
+    
+    num_word_specific_books = Book.objects.filter(title__icontains='animal').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_word_specific_books': num_word_specific_books,
+        'num_visits' : num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='a')[:5] # Get 5 books containing the title war
+    template_name = 'catalog/book_list.html'  # Specify your own template name/location
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    
+    
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+    queryset = Author.objects.filter(last_name__icontains='e')[:5] # Get 5 books containing the title war
+    template_name = 'catalog/author_list.html'  # Specify your own template name/location
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
