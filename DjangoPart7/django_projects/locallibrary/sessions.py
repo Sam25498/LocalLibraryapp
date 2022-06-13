833679a99a3160299daaaf78e092c991f6a1a8a0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-13 13:58:11 +0300

    Create sessions.py

---
+++ DjangoPart7/django_projects/locallibrary/sessions.py
@@ -0,0 +1,11 @@
+# Get a session value by its key (e.g. 'my_car'), raising a KeyError if the key is not present
+my_car = request.session['my_car']
+
+# Get a session value, setting a default if it is not present ('mini')
+my_car = request.session.get('my_car', 'mini')
+
+# Set a session value
+request.session['my_car'] = 'mini'
+
+# Delete a session value
+del request.session['my_car']
