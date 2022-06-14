044500df757514f24979443c4c964cba82236894
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-14 14:30:56 +0300

    Update views.py

--- DjangoPart8/Django_projects/locallibrary/catalog/views.py
+++ DjangoPart8/Django_projects/locallibrary/catalog/views.py
@@ -1,3 +1,81 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
+    num_genres = Genre.objects.filter(name__icontains='f').count()
+    
+    num_word_specific_books = Book.objects.filter(title__icontains='animal').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_word_specific_books': num_word_specific_books,
+        'num_visits' : num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='a')[:5] # Get 5 books containing the title war
+    template_name = 'catalog/book_list.html'  # Specify your own template name/location
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    
+    
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+    queryset = Author.objects.filter(last_name__icontains='e')[:5] # Get 5 books containing the title war
+    template_name = 'catalog/author_list.html'  # Specify your own template name/location
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
@@ -6,7 +84,8 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
-   class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name ='catalog/bookinstance_list_borrowed_user.html'
