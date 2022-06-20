27e9a5f6166bca1ff66f73a453b7f59035386698
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-20 14:30:06 +0300

    Update views.py

--- DjangoPart9/Django_projects/locallibrary/catalog/views.py
+++ DjangoPart9/Django_projects/locallibrary/catalog/views.py
@@ -139,15 +139,18 @@ from django.urls import reverse_lazy
 
 from catalog.models import Author
 
+@permission_required('catalog.can_mark_returned', raise_exception=True)   
 class AuthorCreate(CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     initial = {'date_of_death': '11/06/2020'}
 
+@permission_required('catalog.can_mark_returned', raise_exception=True)   
 class AuthorUpdate(UpdateView):
     model = Author
     fields = '__all__' # Not recommended (potential security issue if more fields added)
 
+@permission_required('catalog.can_mark_returned', raise_exception=True)       
 class AuthorDelete(DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
